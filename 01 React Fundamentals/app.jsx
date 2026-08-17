fa503b98bd34c8f2ba08dde0701587fb7d74573f
Chaitanya Shinde <168747560+chaitanyashinde468@users.noreply.github.com>
2026-08-17 09:47:12 +0530

    Create app.jsx

---
+++ 01 React Fundamentals/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+
+    const name = "Chaitanya";
+
+    return (
+        <>
+            <h1>Welcome {name}</h1>
+
+            <p>
+                Learning React Fundamentals
+            </p>
+        </>
+    );
+}
+
+export default App;
